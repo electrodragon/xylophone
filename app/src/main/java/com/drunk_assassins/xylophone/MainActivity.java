d68140185cf84537d37e5751b7573feec7170793
electrodragon <attaullahkhanlodhi@gmail.com>
2019-12-20 15:27:33 +0500

    Create Project !

---
+++ app/src/main/java/com/drunk_assassins/xylophone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.drunk_assassins.xylophone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
